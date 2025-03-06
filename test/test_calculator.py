1a077779af2d92527f60d81ad527c1bbe293cd3d
Vontor <94050058+Vontor@users.noreply.github.com>
2025-03-06 13:15:37 +0100

    k

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,4 +1,36 @@
 from src import calculator
 
+
+@pytest.mark.paramezrize(
+        "a, b, expected"
+)
 def test_add():
     assert calculator.add(5, 3) == 8
+
+
+def test_add_wrong():
+    assert calculator.add_wrong
+
+
+
+
+def add(a, b):
+    return a + b
+
+def add_wrong(a, b):
+    return a + b + 1
+
+def substract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def multiply_wrong(a, b):
+    return a*b + 1
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Nemužete dělit nulou!!!")
+    else:
+        return a / b
